14f363d1e4266b0e2bda1a61c4a079d8e3b1494d
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2013-07-28 03:26:11 +0200

    message de bienvenue

--- twitter.rb
+++ twitter.rb
@@ -5,10 +5,16 @@ require "pry"
 require "sinatra"
 
 get "/" do
-  binding.pry
   "Qui êtes-vous ?
 
-   <form>
+   <form action='/bienvenue'>
      <input type='text' name='pseudo'>
    </form>"
+end
+
+get "/bienvenue" do
+  pseudo = params["pseudo"]
+  "Bienvenue sur Twitter, #{pseudo} !
+
+   Revenir à l'<a href='/'>accueil</a>"
 end
