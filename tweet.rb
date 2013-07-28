238c68dff72aa05b5ee9654a7aef7126310d2aa2
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2013-07-28 02:34:09 +0200

    possibilité de publier un tweet

---
+++ tweet.rb
@@ -0,0 +1,35 @@
+require 'csv'
+require 'time'
+
+class Tweet
+  attr_accessor :date_complete, :contenu, :pseudo
+
+  def initialize(contenu, pseudo, date_complete = Time.now)
+    @date_complete = date_complete
+    @contenu = contenu
+    @pseudo = pseudo
+  end
+
+  def self.publier(contenu, pseudo, date_complete = Time.now)
+    tweet = new(contenu, pseudo, date_complete)
+    CSV.open("./tweets.csv", "ab") do |csv|
+      csv << [tweet.contenu, tweet.date_complete, tweet.pseudo]
+    end
+    tweet
+  end
+
+  def date
+    @date_complete.strftime("%B %d %Y %H:%M:%S")
+  end
+
+  def self.depuis_csv
+    tweets = []
+    CSV.foreach("./tweets.csv") do |ligne|
+      contenu = ligne[0]
+      date_complete = Time.parse(ligne[1])
+      pseudo = ligne[2]
+      tweets << Tweet.new(contenu, pseudo, date_complete)
+    end
+    tweets.sort_by { |tweet| tweet.date_complete }.reverse
+  end
+end
--- twitter.rb
+++ twitter.rb
@@ -1,4 +1,11 @@
 # encoding: utf-8
+
+require "./tweet.rb"
+
 puts "Qui êtes-vous ?"
 pseudo = gets.strip
 puts "Bienvenue sur Twitter, #{pseudo} !"
+puts "Quoi de neuf ?"
+contenu = gets.strip
+nouveau_tweet = Tweet.publier(contenu, pseudo)
+puts "#{nouveau_tweet.pseudo} : #{nouveau_tweet.contenu} le #{nouveau_tweet.date}"
