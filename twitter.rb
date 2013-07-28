4f1033bdd873fad17c1eccdc48ce062feb2d1a44
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2013-07-28 03:11:42 +0200

    faites entrer Sinatra

--- twitter.rb
+++ twitter.rb
@@ -2,27 +2,8 @@
 
 require "pry"
 
-require "./tweet.rb"
-require "rainbow"
+require "sinatra"
 
-puts "Qui êtes-vous ?"
-pseudo = gets.strip
-puts "Bienvenue sur Twitter, #{pseudo} !"
-puts "Quoi de neuf ?"
-contenu = gets.strip
-nouveau_tweet = Tweet.publier(contenu, pseudo)
-puts "#{nouveau_tweet.pseudo} : #{nouveau_tweet.contenu} le #{nouveau_tweet.date}"
-
-puts "
-
-================================================================================
-                             Bienvenue sur Twitter
-================================================================================
-
-"
-tweets = Tweet.depuis_csv
-
-tweets.each do |tweet|
-  puts "#{tweet.contenu} à #{tweet.date.foreground(:yellow)} par #{tweet.pseudo.background(:blue).foreground(:white)}"
-  puts "-" * 80
+get "/" do
+  "Qui êtes-vous ?"
 end
